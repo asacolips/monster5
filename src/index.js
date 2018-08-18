4de596856e30f51c9a77d0e89d4950e9da5e0dc3
Matt Smith <mattsmith142003@gmail.com>
2018-08-18 16:56:33 -0500

    Add statblock css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 // Monster Calculation
 // http://blogofholding.com/?p=7338
 
+// CSS by https://codepen.io/retractedhack/pen/gPLpWe
+
 // Define elements for ouput. Each element includes
 // a method to calculate the numeric modifier based on
 // the size string (e.g. "smaller" = -2), a method to
@@ -41,7 +43,10 @@ class HitPoints extends React.Component {
   render() {
     const modifier = this.calculateMod(this.props.mod);
     return (
-      <div><strong>HP:</strong> {this.calculate(this.props.cr, modifier)}</div>
+      <div className="property-line property-line--hp">
+        <h4>Hit Points</h4>
+        <p>{this.calculate(this.props.cr, modifier)}</p>
+      </div>
     )
   }
 }
@@ -76,7 +81,10 @@ class ArmorClass extends React.Component {
   render() {
     const modifier = this.calculateMod(this.props.mod);
     return (
-      <div><strong>AC:</strong> {this.calculate(this.props.cr, modifier)}</div>
+      <div className="property-line property-line__ac">
+        <h4>Armor Class</h4>
+        <p>{this.calculate(this.props.cr, modifier)}</p>
+      </div>
     )
   }
 }
@@ -107,7 +115,10 @@ class Attack extends React.Component {
   render() {
     const mod = this.calculateMod(this.props.mod);
     return (
-      <div><strong>Attack:</strong> +{this.calculate(this.props.cr, mod)}</div>
+      <div className="property-line property-line--attack">
+        <h4>Attack:</h4>
+        <p>+{this.calculate(this.props.cr, mod)}</p>
+      </div>
     )
   }
 }
@@ -186,7 +197,7 @@ class Damage extends React.Component {
       multiAttack--;
     }
 
-    attackOutput = multiAttack > 1 ? ' x' + multiAttack : '';
+    attackOutput = multiAttack > 1 ? ' [' + multiAttack + ' attacks]' : '';
 
     return '(' + dieOutput + separator + staticOutput + ')' + attackOutput;
   }
@@ -199,7 +210,10 @@ class Damage extends React.Component {
     const multiAttack = this.props.multiAttack;
     const dieDamage = this.calculateRandomDamage(damage, die, swing, multiAttack);
     return (
-      <div><strong>Damage:</strong> {damage} {dieDamage}</div>
+      <div className="property-line property-line--damage">
+        <h4>Damage:</h4>
+        <p>{damage} {dieDamage}</p>
+      </div>
     )
   }
 }
@@ -230,7 +244,10 @@ class DifficultyClass extends React.Component {
   render() {
     const mod = this.calculateMod(this.props.mod);
     return (
-      <div><strong>Save DC:</strong> {this.calculate(this.props.cr, mod)}</div>
+      <div className="property-line property-line--save-dc">
+        <h4>Save DC:</h4>
+        <p>{this.calculate(this.props.cr, mod)}</p>
+      </div>
     )
   }
 }
@@ -261,7 +278,10 @@ class Save extends React.Component {
   render() {
     const mod = this.calculateMod(this.props.mod);
     return (
-      <div><strong>Save:</strong> +{this.calculate(this.props.cr, mod)}</div>
+      <div className="property-line property-line--save">
+        <h4>Save Bonus:</h4>
+        <p>+{this.calculate(this.props.cr, mod)}</p>
+      </div>
     )
   }
 }
@@ -281,7 +301,7 @@ class ChallengeRatingInput extends React.Component {
     const value = this.props.value;
     const name = this.props.name;
     return (
-      <div>
+      <div className="form-item form-item--text form-item--cr">
         <legend>Enter CR:</legend>
         <input type="number" name={name} value={value}
           onChange={this.handleChange} />
@@ -307,7 +327,7 @@ class ModifierSelect extends React.Component {
     const value = this.props.value;
     const name = this.props.name;
     return (
-      <div>
+      <div className="form-item form-item--select form-item--mod">
         <label htmlFor={name}>{this.props.label}:</label>
         <select name={name} value={value} onChange={this.handleChange}>
           <option value="smaller">Smaller</option>
@@ -336,7 +356,7 @@ class DieSelect extends React.Component {
   render() {
     const value = this.props.value;
     return (
-      <div>
+      <div className="form-item form-item--select form-item--die">
         <label htmlFor="die">Damage Die:</label>
         <select name="die" value={value} onChange={this.handleChange}>
           <option value="3">d3</option>
@@ -365,7 +385,7 @@ class PercentSlider extends React.Component {
   render() {
     const value = this.props.value;
     return (
-      <div>
+      <div className="form-item form-item--range form-item--swing">
         <label htmlFor="swing">Swing:</label>
         <input type="range" name="swing" min="0" max="1" value={value} onChange={this.handleChange} step="0.1" />
         <span>({value * 100}%)</span>
@@ -387,7 +407,7 @@ class MultiAttackSlider extends React.Component {
   render() {
     const value = this.props.value;
     return (
-      <div>
+      <div className="form-item form-item--range form-item--multiattack">
         <label htmlFor="multiAttack">Number of Attacks:</label>
         <input type="range" name="multiAttack" min="1" max="10" value={value} onChange={this.handleChange} step="1" />
       </div>
@@ -435,76 +455,103 @@ class Monster extends React.Component {
     const multiAttack = this.state.multiAttack;
 
     return (
-      <div>
+      <div className="monster">
         {/* Build inputs to collect values. Each value maps a
         generic prop (e.g. value) to a specific state (e.g. cr). */}
-        <fieldset>
-          <ChallengeRatingInput
-            value={cr}
-            name="cr"
-            onChangeEvent={this.handleChange} />
-          <ModifierSelect
-            value={modHp}
-            name="modHp"
-            label="Modify HP"
-            onChangeEvent={this.handleChange} />
-          <ModifierSelect
-            value={modAc}
-            name="modAc"
-            label="Modify AC"
-            onChangeEvent={this.handleChange} />
-          <ModifierSelect
-            value={modAttack}
-            name="modAttack"
-            label="Modify Attack Bonus"
-            onChangeEvent={this.handleChange} />
-          <ModifierSelect
-            value={modDamage}
-            name="modDamage"
-            label="Modify Damage"
-            onChangeEvent={this.handleChange} />
-          <ModifierSelect
-            value={modDc}
-            name="modDc"
-            label="Modify Save DC"
-            onChangeEvent={this.handleChange} />
-          <ModifierSelect
-            value={modSave}
-            name="modSave"
-            label="Modify Saving Throw Bonus"
-            onChangeEvent={this.handleChange} />
-          <DieSelect
-            value={die}
-            onChangeEvent={this.handleChange} />
-          <PercentSlider
-            value={swing}
-            onChangeEvent={this.handleChange} />
-          <MultiAttackSlider
-            value={multiAttack}
-            onChangeEvent={this.handleChange} />
-        </fieldset>
+        <form className="monster__form">
+          <fieldset className="monster__fieldset">
+            <ChallengeRatingInput
+              value={cr}
+              name="cr"
+              onChangeEvent={this.handleChange} />
+            <ModifierSelect
+              value={modHp}
+              name="modHp"
+              label="Modify HP"
+              onChangeEvent={this.handleChange} />
+            <ModifierSelect
+              value={modAc}
+              name="modAc"
+              label="Modify AC"
+              onChangeEvent={this.handleChange} />
+            <ModifierSelect
+              value={modAttack}
+              name="modAttack"
+              label="Modify Attack Bonus"
+              onChangeEvent={this.handleChange} />
+            <ModifierSelect
+              value={modDamage}
+              name="modDamage"
+              label="Modify Damage"
+              onChangeEvent={this.handleChange} />
+            <ModifierSelect
+              value={modDc}
+              name="modDc"
+              label="Modify Save DC"
+              onChangeEvent={this.handleChange} />
+            <ModifierSelect
+              value={modSave}
+              name="modSave"
+              label="Modify Saving Throw Bonus"
+              onChangeEvent={this.handleChange} />
+            <DieSelect
+              value={die}
+              onChangeEvent={this.handleChange} />
+            <PercentSlider
+              value={swing}
+              onChangeEvent={this.handleChange} />
+            <MultiAttackSlider
+              value={multiAttack}
+              onChangeEvent={this.handleChange} />
+          </fieldset>
+        </form>
         {/* Build markup to output values. */}
-        <HitPoints
-          cr={cr}
-          mod={modHp} />
-        <ArmorClass
-          cr={cr}
-          mod={modAc} />
-        <Attack
-          cr={cr}
-          mod={modAttack} />
-        <Damage
-          cr={cr}
-          mod={modDamage}
-          die={die}
-          swing={swing}
-          multiAttack={multiAttack} />
-        <DifficultyClass
-          cr={cr}
-          mod={modDc} />
-        <Save
-          cr={cr}
-          mod={modSave} />
+        <div className="stat-block">
+          <hr className="orange-border" />
+          <div className="creature-heading">
+            <h1>Dangerous Foe!</h1>
+            <h2>Medium monstrosity, unaligned</h2>
+          </div>
+          <svg height="5" width="100%" class="tapered-rule">
+            <polyline points="0,0 400,2.5 0,5"></polyline>
+          </svg>
+          <div className="top-stats">
+            <ArmorClass
+              cr={cr}
+              mod={modAc} />
+            <HitPoints
+              cr={cr}
+              mod={modHp} />
+          </div>
+          <svg height="5" width="100%" class="tapered-rule">
+            <polyline points="0,0 400,2.5 0,5"></polyline>
+          </svg>
+          <div className="bottom-stats">
+            <DifficultyClass
+              cr={cr}
+              mod={modDc} />
+            <Save
+              cr={cr}
+              mod={modSave} />
+            <div className="property-line property-line--cr">
+              <h4>Challenge</h4>
+              <p>{cr}</p>
+            </div>
+          </div>
+          <svg height="5" width="100%" class="tapered-rule">
+            <polyline points="0,0 400,2.5 0,5"></polyline>
+          </svg>
+          <Attack
+            cr={cr}
+            mod={modAttack} />
+          <Damage
+            cr={cr}
+            mod={modDamage}
+            die={die}
+            swing={swing}
+            multiAttack={multiAttack} />
+            <hr className="orange-border bottom" />
+          </div>
       </div>
     );
   }
